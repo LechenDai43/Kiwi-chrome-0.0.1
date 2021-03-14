015feb00eb54f8d95b2c6d57b4abffadd7df5009
ConnorPeng <pjh_jinghong@outlook.com>
2021-03-13 20:21:40 -0500

    pjh_kiwi_header
    Kiwi header

---
+++ app/components/Kiwi_header.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class Kiwi_header extends Component{
+    state = {};
+
+    render() {
+        return (
+        <div>
+        <span>Kiwi</span>
+        </div>
+        );
+    }
+}
