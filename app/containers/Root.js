d5c5f71d567984fa1bc86248d6d280c79422e870
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-03-16 20:27:52 -0400

    Debug selectionpage

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,7 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 import MainPage from "./Main/MainPage";
-// import SelectionPage from "./Selection/SelectionPage";
+import SelectionPage from "./Selection/SelectionPage";
+import ReviewContainer from "./Review/ReviewContainer";
 
 export default class Root extends Component {
 
--- app/containers/Selection/SelectionPage.js
+++ app/containers/Selection/SelectionPage.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component} from 'react';
 import { connect } from 'react-redux';
 import Header from "../../components/Header/Header";
 import Selector from "../../components/Selector/Selector";
@@ -7,12 +7,12 @@ import Selector from "../../components/Selector/Selector";
 export default class SelectionPage extends Component {
     populateStore() {
         return (
-            <Selector>
+            <Selector/>
         );
     }
 
     render() {
-        return (
+       return (
             <div className="selection-page-body-leo">
                 <Header/>
                 <h3 className="selection-instruction">Select your favorite stores</h3>
@@ -23,16 +23,5 @@ export default class SelectionPage extends Component {
                 // Here should be the finish button
             </div>
         );
-        // return (
-        //     <div className="selection-page-body-leo">
-        //         <h1>Kiwi Header</h1>
-        //         <h3 className="selection-instruction">Select your favorite stores</h3>
-        //         <hr/>
-        //         <div className="list-of-stores">
-        //             {this.populateStore()}
-        //         </div>
-        //         // Here should be the finish button
-        //     </div>
-        // );
     }
 }
