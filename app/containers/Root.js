7106361aa7298cbcc0b36831552a64339801f6b5
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-03-21 15:02:31 -0400

    from selection page to main page

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -5,13 +5,53 @@ import MainPage from './Main/MainPage';
 import SelectionPage from './Selection/SelectionPage';
 import ReviewContainer from './Review/ReviewContainer';
 
+const possible_page = ['Select', 'Main', 'Review', 'Profile'];
+
 export default class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'Select'
+    };
+  }
+
+  fromSelection2Main () {
+    this.setState({page: 'Main'});
+    this.forceUpdate();
+  }
+
+  redirectByFooter (targetPage) {
+    var valid = 0;
+    for (var i = 0; i < possible_page.length; i++) {
+      if (targetPage === possible_page[i]) {
+        valid = 1;
+        break;
+      }
+    }
+    if (valid === 1) {
+      this.setState({page: targetPage});
+      this.forceUpdate();
+    }
+  }
+
+  pickPage() {
+    if (this.state.page === 'Select') {
+      return (<SelectionPage redirection={() => this.fromSelection2Main()}/>);
+    }
+    else if (this.state.page === 'Main') {
+      return (<MainPage redirection={() => this.redirectByFooter()}/>)
+    }
+    else {
+      return (<h1>Wait</h1>);
+    }
+
+  }
 
   render() {
     const { store } = this.props;
     return (
       <Provider store={store} style={{ width: '200px', height: '500px' }}>
-        <MainPage />
+        {this.pickPage()}
       </Provider>
     );
   }
